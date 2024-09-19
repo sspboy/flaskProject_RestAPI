dfcbbe42c0967b93bbbcd817c72adbf1240b2798
shaosp <sspboy@163.com>
2024-09-19 18:32:15 +0800

    '20240919'

--- API_/resources/BasicSettings/Model_Role.py
+++ API_/resources/BasicSettings/Model_Role.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource                      # 接口处理方法
 from API_.DB.DB_model import Basic_Operations           # 数据查询方法
 from flask import request
+from flask_login import login_required,current_user
 import json
 
 
@@ -12,19 +13,22 @@ class _list:
         # 数据表头名称、数据类型、描述说明
         self.DataColumn =[
             {
+                "key": "1",
                 "field_name": "id",   # 字段名称
                 "field_type": "int",    # 字段类型
-                "remark": "数据唯一id",      # 备注描述
+                "title": "数据唯一id",      # 备注描述
+                "dataIndex": "id",  # 数据索引
+
             },
-            {"field_name": "b_id", "field_type": "int", "remark": "品牌id"},
-            {"field_name": "role_name", "field_type": "str", "remark": "角色名称"},
-            {"field_name": "role_info", "field_type": "json", "remark": "角色描述"},
-            {"field_name": "role_state", "field_type": "int", "remark": "停用状态"},
-            {"field_name": "view_permissions", "field_type": "int", "remark": "视图权限"},
-            {"field_name": "data_permissions", "field_type": "int", "remark": "数据权限"},
-            {"field_name": "fun_permissions", "field_type": "int", "remark": "功能权限"},
-            {"field_name": "create_time", "field_type": "timestamp", "remark": "创建时间"},
-            {"field_name": "update_time", "field_type": "timestamp", "remark": "更新时间"}
+            {"key": "2","field_name": "b_id", "field_type": "int", "title": "品牌id","dataIndex": "b_id",},
+            {"key": "3","field_name": "role_name", "field_type": "str", "title": "角色名称","dataIndex": "role_name",},
+            {"key": "4","field_name": "role_info", "field_type": "json", "title": "角色描述","dataIndex": "role_info",},
+            {"key": "5","field_name": "role_state", "field_type": "int", "title": "停用状态","dataIndex": "role_state",},
+            {"key": "6","field_name": "view_permissions", "field_type": "int", "title": "视图权限","dataIndex": "view_permissions",},
+            {"key": "7","field_name": "data_permissions", "field_type": "int", "title": "数据权限","dataIndex": "data_permissions",},
+            {"key": "8","field_name": "fun_permissions", "field_type": "int", "title": "功能权限","dataIndex": "fun_permissions",},
+            {"key": "9","field_name": "create_time", "field_type": "timestamp", "title": "创建时间","dataIndex": "create_time",},
+            {"key": "10","field_name": "update_time", "field_type": "timestamp", "title": "更新时间","dataIndex": "update_time",}
         ]
 
     # 获取数据表的列名称
@@ -121,6 +125,12 @@ class RoleList(Resource):
         data_list = res.get('data')
 
         res['data'] = _list().re_data_list_name(data_list)
+
+        # 用户信息
+        res['user'] = current_user.user_obj
+
+        # 表头信息
+        res['colum'] = _list().DataColumn
 
         return res
 
